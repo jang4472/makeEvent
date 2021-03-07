bb4831b30c1bfef9c4aec7d5f9ed727933316641
jang4472 <jang4472@gmail.com>
2021-03-07 17:31:25 +0900

    ArticleDao 생성 및 적용

---
+++ src/main/java/com/harunice/makeEvent/dao/ArticleDao.java
@@ -0,0 +1,9 @@
+package com.harunice.makeEvent.dao;
+
+import java.util.List;
+
+import com.harunice.makeEvent.dto.Article;
+
+public interface ArticleDao {
+	public List<Article> getList();
+}
--- src/main/java/com/harunice/makeEvent/service/ArticleServiceImpl.java
+++ src/main/java/com/harunice/makeEvent/service/ArticleServiceImpl.java
@@ -3,23 +3,19 @@ package com.harunice.makeEvent.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.harunice.makeEvent.dao.ArticleDao;
 import com.harunice.makeEvent.dto.Article;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
+	@Autowired
+	ArticleDao articleDao;
+	// 이제 ArticleService는 데이터 관련해서는 모두 Dao에게 위임
+	
 	public List<Article> getList() {
-		Article article1 = new Article(1L, "2021-03-07 15:35:00", "제목1", "내용1");
-		Article article2 = new Article(2L, "2021-03-07 15:35:00", "제목2", "내용2");
-		Article article3 = new Article(3L, "2021-03-07 15:35:00", "제목3", "내용3");
-		
-		List<Article> list = new ArrayList<>();
-		
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
-		
-		return list;
+		return articleDao.getList();
 	}	
 }
